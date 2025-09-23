a1c524ce4c74b16c3887e25330478c50333e418e
SAMIR <158018273+xerinv0@users.noreply.github.com>
2025-09-23 09:27:02 +0000

    - plugin folder

--- App/Core/Plugin/PluginInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Core\Plugin;
-
-interface PluginInterface {
-    public function onActivate(): void;
-    public function onDeactivate(): void;
-    public function getManifest(): array;
-}
